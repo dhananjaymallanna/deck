147a34344f67b6e11227ffdb4f15ac5506ed0ec9
Chris Thielen <github@sandgnat.com>
2020-02-06 16:38:21 -0800

    use stderr for informational stuff use stdout for result

--- app/scripts/modules/assert_package_bumps_standalone.sh
+++ app/scripts/modules/assert_package_bumps_standalone.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-# Usage: assert_pacakge_bumps_standalone [target branch]
+# Usage: assert_package_bumps_standalone [target branch]
 # Use [target branch] to change the branch the script checks for changes against
 # (default: origin/master if running in a Github Action, master otherwise)
 
+error() {
+  echo $* >&2
+}
+
+verbose() {
+  [[ $VERBOSE == "true" ]] && echo $*
+}
+
 # Reports if package bumps are combined with other changes (not allowed). Package bumps must be standalone.
 if [[ $GITHUB_ACTIONS == "true" && ( $GITHUB_BASE_REF != "master" || $GITHUB_REPOSITORY != 'spinnaker/deck' ) ]] ; then
-  echo "Not a pull request to master, exiting"
+  error "Not a pull request to master -- exiting"
   exit 0
 fi
 
+if [[ "$1" == "--verbose" ]] ; then
+  VERBOSE=true
+fi
 
 if [[ $GITHUB_ACTIONS == "true" ]] ; then
-  echo "Fetching tags..." && git fetch -q
+  verbose "Fetching tags..." && git fetch -q
   GHA_TARGET=origin/master
   cd app/scripts/modules || exit 1;
 else
@@ -23,69 +34,66 @@ TARGET_BRANCH=${1}
 TARGET_BRANCH=${TARGET_BRANCH:-${GHA_TARGET}}
 TARGET_BRANCH=${TARGET_BRANCH:-master}
 
-PKGJSONCHANGED="Version change detected in package.json"
-ONLYVERSIONCHANGED="Version change must be the only line changed in package.json"
+PKGJSONCHANGED="Version change detected"
+ONLYVERSIONCHANGED="Other changes were found in package.json"
 ONLYPKGJSONCHANGED="package.json (in app/scripts/modules) must be the only files changed in a pull request with version bumps"
 
-echo "TARGET_BRANCH=$TARGET_BRANCH"
+error "TARGET_BRANCH=$TARGET_BRANCH"
 
 # Tests are run against an ephemeral merge commit so we don't have to merge in $TARGET_BRANCH
 
-PUREBUMP=none
+PUREBUMPS=""
+NOTBUMPED=""
 for PKGJSON in */package.json ; do
   MODULE=$(basename "$(dirname "$PKGJSON")")
-  echo "::group::Checking $MODULE"
 
-  echo "==================================================="
-  echo "Checking $MODULE"
-  echo "==================================================="
   HAS_PKG_BUMP=$(git diff "$TARGET_BRANCH" -- "$PKGJSON" | grep '"version"' | wc -l)
-  [[ $? -ne 0 ]] && exit 2
-  if [ "$HAS_PKG_BUMP" -ne 0 ] ; then
-    echo " [ YES  ] $PKGJSONCHANGED"
+  [[ $? -ne 0 ]] && exit 2 # Exit if git command fails
 
+  if [ "$HAS_PKG_BUMP" -ne 0 ] ; then
     # Ensuring that the version change is the only change in package.json
     PKG_JSON_OTHER_CHANGES=$(git diff --numstat "$TARGET_BRANCH" -- "$PKGJSON" | cut -f 1)
     if [ "$PKG_JSON_OTHER_CHANGES" -ne 1 ] ; then
-      echo " [ FAIL ] $ONLYVERSIONCHANGED"
-      echo ""
-      echo "=========================================="
-      echo "Failure details (git diff of package.json)"
-      echo "=========================================="
-      echo ""
-      git diff "$TARGET_BRANCH" -- "$PKGJSON"
-      echo ""
-      echo "=========================================="
+      error "==================================================="
+      error " $PKGJSONCHANGED in $MODULE/package.json"
+      error ""
+      git diff -u "$TARGET_BRANCH" -- "$PKGJSON" | grep '"version"' >&2
+      error ""
+      error " However, other changes were found in package.json"
+      error " (git diff of package.json)"
+      error "=========================================="
+      git diff "$TARGET_BRANCH" -- "$PKGJSON" >&2
+      error "=========================================="
       exit 3
     fi
 
-    echo " [ PASS ] $ONLYVERSIONCHANGED"
 
     # checking that the only files changed are app/scripts/modules/*/package.json
     OTHER_FILES_CHANGED=$(git diff --name-only "$TARGET_BRANCH" | grep -v "app/scripts/modules/.*/package.json" | wc -l)
     [[ $? -ne 0 ]] && exit 4
     if [ "$OTHER_FILES_CHANGED" -ne 0 ] ; then
-      echo " [ FAIL ] $ONLYPKGJSONCHANGED"
-      echo ""
-      echo "==========================================="
-      echo "Failure details (list of all files changed)"
-      echo "==========================================="
-      echo ""
-      git diff --name-only "$TARGET_BRANCH"
-      echo ""
-      echo "==========================================="
+      error "==================================================="
+      error " $PKGJSONCHANGED in $MODULE/package.json"
+      error ""
+      git diff -u "$TARGET_BRANCH" -- "$PKGJSON" | grep '"version"' >&2
+      error ""
+      error " However, other files were also changed"
+      error " (list of all files changed)"
+      error "=========================================="
+      git diff --name-only "$TARGET_BRANCH" >&2
+      error "=========================================="
       exit 5
     fi
-    # at least one pure bump found
-    PUREBUMP=true
-    echo " [ PASS ] $ONLYPKGJSONCHANGED"
-    echo "::endgroup::"
+
+    PUREBUMPS="$PUREBUMPS $MODULE"
   else
-    echo " [  NO  ] $PKGJSONCHANGED"
-    echo " [  N/A ] $ONLYVERSIONCHANGED"
-    echo " [  N/A ] $ONLYPKGJSONCHANGED"
+    NOTBUMPED="$NOTBUMPED $MODULE"
   fi
-  echo ""
 done
 
-export PUREBUMP
+if [[ $VERBOSE == "true" ]] ; then
+  [[ -n $PUREBUMPS ]] && echo "Pure Package Bumps: $PUREBUMPS"
+  [[ -n $NOTBUMPED ]] && echo "Packages not bumped: $NOTBUMPED"
+else
+  [[ -n $PUREBUMPS ]] && echo "pure" || echo "nobump";
+fi
